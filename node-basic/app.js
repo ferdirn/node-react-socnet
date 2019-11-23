574c6b860d6c6fd2976c0a5c966ba0b77ebcd95e
Ferdi Ramdhon <ferdi@hyperjump.tech>
2019-11-23 19:49:03 +0700

    add http server

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,4 +1,11 @@
 const {sum} = require('./helpers');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end("Hello World from Node JS");
+});
+
+server.listen(3000);
 
 const total = sum(12, 200);
 console.log("TOTAL:", total);
